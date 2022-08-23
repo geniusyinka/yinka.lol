a0009d23b3627ab291399a8112e7985d286884b1
yunggenius <yunggenius4@gmail.com>
2022-08-24 00:58:57 +0100

    thoughts component

---
+++ pages/thoughts.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import Thoughts from '../component/Thoughts';
+import styles from '../styles/Home.module.css'
+
+
+const thoughts: NextPage = () => {
+
+  return (
+    <Thoughts/>
+  )
+}
+
+export default Thoughts;
