26485369a372dcdf7ad16f7efb54ee4a653d3338
geniusyinka <yunggenius4@gmail.com>
2023-06-08 01:09:33 +0100

    looking steezyyy

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,30 +14,42 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <div className='links inline'>
+
       <main className={styles.main}>
         <h1 className=" text-5xl">Hi, I'm Yinka</h1>
         <br />
         <p>I enjoy building tools, and simplifying technical materials. </p>
         <br />
-
+        {/* <h1 className=" text-3xl">Work</h1>
+        <br />
+        <ul>
+        <li>
+        Dev Rel 
+        </li>
+      </ul> */}
         <p>
-          I am a Crypto Native/Developer Relations Engineer. Building the dev
+          I am a Crypto Native/Developer Relations Engineer. <br />
+          Currently building the dev
           community{" "}
           <a
             href="https://twitter.com/thedapplist"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            <b>@thedapplist.</b></a> Dev Rel <a
+            className='urls'
+            
+            >
+            <b>@thedapplist.</b></a> Carrying out Dev Rel work <a
               href="https://twitter.com/developer_DAO"
               target="_blank"
               rel="noopener noreferrer"
-            > <b>@Developer DAO</b></a>.
+              className='urls'
+              > <b>@Developer DAO</b></a>.
           
         
           <br />I am also quite curious about Zero Knowledge Proofs & AI. I talk
           about them here:{" "}
-          <a href="https://www.youtube.com/@geniusyinka">
+          <a href="https://www.youtube.com/@geniusyinka" >
             <b>Here</b>
           </a>
           .
@@ -45,7 +57,7 @@ const Home: NextPage = () => {
         <br />
         <br />
 
-        <div className=" box items-center">
+        <div className=" box links items-start">
           {/* <a href="/thoughts"
             rel="noopener noreferrer"
             className={styles.card}>
@@ -53,7 +65,7 @@ const Home: NextPage = () => {
           </a> */}
 
           <Link href="/thoughts">
-            <a rel="noopener noreferrer" className="">
+            <a rel="noopener noreferrer" className='urls'>
               <h2>thoughts &rarr;</h2>
             </a>
           </Link>
@@ -67,15 +79,17 @@ const Home: NextPage = () => {
             href="https://youtube.com/wilsonoshidipe"
             target="_blank"
             rel="noopener noreferrer"
-          >
+            className='urls' 
+            >
             <h2>youtube &rarr;</h2>
           </a>
 
-          <a href="https://github.com/yunggenius/yinka.lol" className="">
+          <a href="https://github.com/yunggenius/yinka.lol" className='urls' >
             <h2>github &rarr;</h2>
           </a>
         </div>
       </main>
+            </div>
 
       <footer className={styles.footer}>Powered by shitpostin</footer>
     </div>
